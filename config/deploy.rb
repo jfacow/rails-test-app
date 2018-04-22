a3f582160fb9b17fa09827a9364a13fc16493074
ilya <jfacow@gmail.com>
2018-04-22 16:23:33 +0000

    nothing

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,8 +48,8 @@ set :user, "deploy"
 #before "deploy:assets:precompile", "deploy:yarn_install"
 
 namespace :deploy do
-  desc 'Run rake yarn:install'
-  task :yarn_install do
+  desc 'Run yarn:add:prod'
+  task :yarn_add do
     on roles(:web) do
       within release_path do
 #        execute("cd #{release_path} && yarn install")
