64ee693645543c7d92ce91b2e93e1fe531e48594
ilya <jfacow@gmail.com>
2018-04-22 16:20:01 +0000

    nothing

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :user, "deploy"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-before "deploy:assets:precompile", "deploy:yarn_install"
+#before "deploy:assets:precompile", "deploy:yarn_install"
 
 namespace :deploy do
   desc 'Run rake yarn:install'
